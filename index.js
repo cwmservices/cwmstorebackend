c27fa79eb080c569361cea03898d6f91533a20a4
Masood4488 <Masoodkhan224488@gmail.com>
2022-09-30 14:27:47 +0500

    improved second time

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     credentials: true,
-    origin: "https://cwmstore.netlify.app/",
+    origin: "https://cwmstore.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
