e583c8e2eab22410adb82c1d185a309b1e1875fb
Masood Ur Rehman <76917820+cwmservices@users.noreply.github.com>
2023-01-16 23:19:07 +0500

    updated Connection
    Added exports

--- db/conn.js
+++ db/conn.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 
 dbURL = process.env.URL;
 
-export const connectDB = async () => {
-mongoose
-  .connect(dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(dbURL, {useNewUrlParser: true,useUnifiedTopology: true,});  
     console.log("Connection to database has been successful");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  }
+  catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
+
+module.exports = { connectDB };
+
